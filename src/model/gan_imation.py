3618333edd8fed8d33a6c86f6ce86bd6599b3363
tungduong123q@gmail.com <tungduong123q@gmail.com>
2025-02-14 00:45:30 +0700

    refactor the formula for generated image

--- src/model/gan_imation.py
+++ src/model/gan_imation.py
@@ -17,13 +17,13 @@ class GANImation(nn.Module):
         
     def forward(self, image, origin_au_vec, final_au_vec):
         # generated image of generator
-        image_reg_origin_final, atttention_reg_origin_final = self.gen_origin_final(image, origin_au_vec)
-        gen_image_origin_final = image_reg_origin_final * atttention_reg_origin_final + (1 - image_reg_origin_final) * image
+        image_reg_origin_final, attention_reg_origin_final = self.gen_origin_final(image, origin_au_vec)
+        gen_image_origin_final = image_reg_origin_final * attention_reg_origin_final + (1 - attention_reg_origin_final) * image
         # the input pass through the discriminator
         out_real, out_aux = self.discriminator(gen_image_origin_final)
         # the generated image is concatinated with the final au vector and pass through the second
         # generator for get the original image
         image_reg_final_origin, attention_reg_final_origin = self.gen_final_origin(gen_image_origin_final, final_au_vec)
-        gen_image_final_origin = image_reg_final_origin * attention_reg_final_origin + (1 - image_reg_final_origin) * gen_image_origin_final
+        gen_image_final_origin = image_reg_final_origin * attention_reg_final_origin + (1 - attention_reg_final_origin) * gen_image_origin_final
         
         return gen_image_origin_final, gen_image_final_origin, out_real, out_aux
